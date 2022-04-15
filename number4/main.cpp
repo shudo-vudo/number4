fb65dc925e2c752d2addac34464ecec85045f3b9
klimas <dj121jd@gmail.com>
2022-04-15 23:50:15 +0300

    Добавьте файлы проекта.

---
+++ number4/main.cpp
@@ -0,0 +1,10 @@
+﻿#include "osnova.h"
+
+using namespace std;
+
+int main()
+{
+	double a, b, n, m;
+	cin >> a >> b;
+	result(a, b);
+}
---
+++ number4/osnova.h
@@ -0,0 +1,12 @@
+#ifndef OSNOVA_H
+#define OSNOVA_H
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void result(double, double);
+
+
+#endif
